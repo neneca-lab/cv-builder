0d29cfa58d26336a209561d125942862fe8c8a13
neneca-lab <hu.patatyy@gmail.com>
2025-07-15 11:42:01 -0300

    feat: adiciona nova rota para init

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CurriculoController;
+use App\Http\Controllers\DocumentoController;
 use App\Http\Controllers\EnderecoController;
 use App\Http\Controllers\UploadController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('init', function () {
+    return view('init');
+})->name('init');
+
+
+Route::get('/init', [DocumentoController::class, 'index'])->name('init');
 
 
 Route::post('/curriculo', [CurriculoController::class, 'store'])->name('curriculo.store');
 Route::get('/consulta-cep/{cep}', [EnderecoController::class, 'buscarEndereco']);
+Route::get('/pdf/download/{arquivo}', [DocumentoController::class, 'download'])->name('download');
 
 
 
